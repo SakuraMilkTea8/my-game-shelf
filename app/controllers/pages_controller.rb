48ccd0ebf54a5f72f3908e99b54722847ee39c36
mikebennett <mvbennett9@gmail.com>
2022-08-04 21:41:59 +0900

    made seeds for a single user and all the games

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  # would we be able to use this method to get lists made on sign up?
+  def create_lists
+    List.create(name: 'completed', user_id: current_user.id)
+    List.create(name: 'currently playing', user_id: current_user.id)
+    List.create(name: 'want to play', user_id: current_user.id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+if ENV['minimal'] == 'yes'
+  Game.destroy_all
+
+  url = "https://api.rawg.io/api/games?key=#{ENV[RAWG_API]}"
+  doc = URI.parse(url).open.read
+  response = JSON.parse(doc)
+  games = response['results']
+  games.take(20).each do |game|
+    genres = []
+    game['genres'].each { |hash| genres << hash['name'] }
+    platforms = []
+    game['platforms'].each { |hash| platforms << hash['platform']['name'] }
+    this = Game.new(title: game['name'], genre: genres.join(" "), developer: "Hamish Liu", console: platforms.join(" "), price: 3.5, release_date: game['released'], image_url: game['background_image'])
+    this.save
+    p this.title
+  end
+end
+
+puts 'Makin that seedy boi'
+
+seedyboi = User.new(email: 'seedy@seed.com', password: '123456', name: 'Seedy Seed Boi')
+seedyboi.save!
+
+p seedyboi.name
+
+List.create(name: 'completed', user_id: seedyboi.id)
+List.create(name: 'currently playing', user_id: seedyboi.id)
+List.create(name: 'want to play', user_id: seedyboi.id)
