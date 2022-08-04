de960ddd8409276ca47b547665b8c6c8a186c6a5
hamish liu <hamish.mc.liu@gmail.com>
2022-08-04 20:53:46 +0900

    this will need to be changed but some validations have been added

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
 class Game < ApplicationRecord
   has_many :reviews
+  has_many :list_games
+
+  validates :title, presence: true
+  validates :name, uniqueness: true
+  validates :genre, presence: true
+  validates :developer, presence: true
+  validates :console, presence: true
+  validates :price, presence: true
+  validates :release_date, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :content, length: { minimum: 6 }
+
+  validates :content, presence: true
+
+  validates :rating, presence: true
 end
