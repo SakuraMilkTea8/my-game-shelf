a4586a653215ba78a63953d38f3cf613a303c197
mikebennett <mvbennett9@gmail.com>
2022-08-12 22:51:11 +0900

    cards line up a bit better

--- app/javascript/controllers/shelf_categories_controller.js
+++ app/javascript/controllers/shelf_categories_controller.js
@@ -4,19 +4,27 @@ export default class extends Controller {
   static targets = ["want", "playing", "complete", "wants", "plays", "completes"]
 
   want() {
-    this.wantTarget.backgroundColor = 'black'
+    this.wantTarget.classList.add('selected-category')
+    this.playingTarget.classList.remove('selected-category')
+    this.completeTarget.classList.remove('selected-category')
     this.playsTarget.setAttribute('hidden', 'true')
     this.completesTarget.setAttribute('hidden', 'true')
     this.wantsTarget.removeAttribute('hidden')
   }
 
   playing() {
+    this.playingTarget.classList.add('selected-category')
+    this.wantTarget.classList.remove('selected-category')
+    this.completeTarget.classList.remove('selected-category')
     this.wantsTarget.setAttribute('hidden', 'true')
     this.completesTarget.setAttribute('hidden', 'true')
     this.playsTarget.removeAttribute('hidden')
   }
 
   completed() {
+    this.completeTarget.classList.add('selected-category')
+    this.playingTarget.classList.remove('selected-category')
+    this.wantTarget.classList.remove('selected-category')
     this.wantsTarget.setAttribute('hidden', 'true')
     this.playsTarget.setAttribute('hidden', 'true')
     this.completesTarget.removeAttribute('hidden')
