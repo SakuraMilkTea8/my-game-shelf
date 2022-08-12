11242053ded23c66f518d684e971c16f33887eae
mikebennett <mvbennett9@gmail.com>
2022-08-12 22:23:16 +0900

    cards line up neatly when less than max

--- app/javascript/controllers/shelf_categories_controller.js
+++ app/javascript/controllers/shelf_categories_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   static targets = ["want", "playing", "complete", "wants", "plays", "completes"]
 
   want() {
+    this.wantTarget.backgroundColor = 'black'
     this.playsTarget.setAttribute('hidden', 'true')
     this.completesTarget.setAttribute('hidden', 'true')
     this.wantsTarget.removeAttribute('hidden')
