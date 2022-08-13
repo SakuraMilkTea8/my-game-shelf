50601fc843441c5e154b909bc3aac057c0c8b677
hamishliu <hamish.mc.liu@gmail.com>
2022-08-13 16:27:22 +0900

    can view reviews on your profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   before_action :verify_authorized, except: [:show]
 
   def show
+    @games = Game.all
+    @game = Game.find(params[:id])
     @user = User.find(params[:id])
     authorize @user
   end
