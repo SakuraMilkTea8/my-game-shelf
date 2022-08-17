8c40624c72a3efbe39ef69bc88bb7d380b70af91
hamishliu <hamish.mc.liu@gmail.com>
2022-08-17 14:03:36 +0900

    users now have to have a name when they sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
   has_many :shelf_games, dependent: :destroy
   has_many :games, through: :shelf_games
+
+  validates :name, presence: true
 end
