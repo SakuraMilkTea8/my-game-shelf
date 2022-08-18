836858027d66095c3cc1f7a554f81734fdcc5b38
hamishliu <hamish.mc.liu@gmail.com>
2022-08-18 11:18:26 +0900

    got shelf data for my users profile and added the chart.js yarn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # users can follow other users
     @users = User.all
+    # for the shelf list on users profile
+    @want_to_play = []
+    @now_playing = []
+    @completed = []
+    @user.shelf_games.each do |game|
+      if game.category == "completed"
+          @completed << game.category
+          @completed_count = @completed.count
+      elsif game.category == "want to play"
+          @want_to_play << game
+          @want_to_play_count = @want_to_play.count
+      else game.category == "now playing"
+          @now_playing << game
+          @now_playing_count = @now_playing.count
+      end
+    end
+      # raise
     authorize @user
   end
 end
