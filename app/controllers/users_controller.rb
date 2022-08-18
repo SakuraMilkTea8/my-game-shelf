825984abcb23a03c3e56ef3bb58d0d1087faeb55
hamishliu <hamish.mc.liu@gmail.com>
2022-08-18 15:08:54 +0900

    added the array of games to the graph but the styling is super weird

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,13 +16,14 @@ class UsersController < ApplicationController
       if game.category == "completed"
           @completed << game.category
           @completed_count = @completed.count
-          @completed_json = @completed_count.to_json
       elsif game.category == "want to play"
           @want_to_play << game
           @want_to_play_count = @want_to_play.count
       else game.category == "now playing"
           @now_playing << game
           @now_playing_count = @now_playing.count
+          @graph_array = [@completed_count, @want_to_play_count, @now_playing_count]
+          # raise
       end
     end
       # raise
