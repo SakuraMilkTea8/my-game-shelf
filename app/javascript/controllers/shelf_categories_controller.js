9ae75bb06f8a37127bfa779efbcd27029752ddca
mikebennett <mvbennett9@gmail.com>
2022-08-27 16:20:00 +0900

    got controllers ready

--- app/javascript/controllers/shelf_categories_controller.js
+++ app/javascript/controllers/shelf_categories_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["want", "playing", "complete", "wants", "plays", "completes"]
+  static targets = ["want", "playing", "complete", "wants", "plays", "completes", "form", "shelfcard"]
 
   want() {
     this.wantTarget.classList.add('selected-category')
@@ -30,8 +30,29 @@ export default class extends Controller {
     this.completesTarget.removeAttribute('hidden')
   }
 
+  updateCategory(event) {
+    event.preventDefault();
+    // console.log(event.currentTarget.id);
+    // destination: event.currentTarget[1].value
+    // edit_shelf_game_ event.currentTarget.id
+    let cardIdArray = event.currentTarget.id.split('_')
+    let cardId = cardIdArray[cardIdArray.length - 1]
+    // console.log(cardId);
+    let shelfCard = this.shelfcardTargets.find(e => e.id === cardId)
+    // console.log(shelfCard);
+    if (event.currentTarget[1].value === 'want to play') {
+
+    } else if (event.currentTarget[1].value === 'now playing') {
+
+    } else if (event.currentTarget[1].value === 'completed') {
+
+    }
+  }
+
   connect() {
     this.wantTarget.classList.add('selected-category')
     this.wantsTarget.removeAttribute('hidden')
+    console.log(this.formTargets);
+    console.log(this.shelfcardTargets);
   }
 }
