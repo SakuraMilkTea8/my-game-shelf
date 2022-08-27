807204c4f09133fad2ccd8d9f7f11ac980758851
mikebennett <mvbennett9@gmail.com>
2022-08-27 15:07:38 +0900

    added a log for target

--- app/javascript/controllers/shelf_buttons_controller.js
+++ app/javascript/controllers/shelf_buttons_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // static targets = ["items", "form"]
+  static target = "form"
 
   connect() {
     console.log(this.element.id)
+    console.log(this.formTarget);
   }
 }
