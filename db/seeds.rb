7dfb7aaeabe34e1bae3364ad8cad1f04af948909
Mike Bennett <mvbennett9@gmail.com>
2022-08-30 21:33:50 +0900

    review seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -102,6 +102,32 @@ seedy_game.save!
 
 p "#{seedy_game.user.name} added #{seedy_game.game.title} to their shelf under #{seedy_game.category}"
 
+# reviews
+
+content = "wow amazing game, it really changed my life"
+all_users = User.all
+all_games = Game.all
+all_users.each do |user|
+  review = Review.new(content: content, rating: rand(4..5), user: user, game: all_games.sample)
+  review.save!
+end
+
+content = "meeeeeh"
+all_users = User.all
+all_games = Game.all
+all_users.each do |user|
+  review = Review.new(content: content, rating: rand(2..3), user: user, game: all_games.sample)
+  review.save!
+end
+
+content = "I'd rather play Duke Nukem Forever"
+all_users = User.all
+all_games = Game.all
+all_users.each do |user|
+  review = Review.new(content: content, rating: 1, user: user, game: all_games.sample)
+  review.save!
+end
+
 # require 'google/apis/youtube_v3'
 # require 'active_support/all'
 # GOOGLE_API_KEY=ENV['GOOGLE_API_KEY']
