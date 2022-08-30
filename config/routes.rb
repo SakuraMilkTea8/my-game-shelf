0631c00783d2ab6c654a2baa7d7a6ca3286cf6fb
Hamish Liu <99053023+beenzino48@users.noreply.github.com>
2022-08-30 22:45:29 +0900

    Revert "Game adding buttons"

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :shelf_games, only: [:index, :destroy]
+  resources :shelf_games, only: [:index, :create, :update, :destroy]
   root to: 'games#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # If we decide to have user created shelves we should add :new and :create
 
   resources :games, only: [:index, :show] do
-    resources :shelf_games, only: [:create, :update]
-
     resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
 end
