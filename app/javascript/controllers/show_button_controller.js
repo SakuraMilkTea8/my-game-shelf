13ed5d906a87ab00f57c033da71c4ae0b2f4c3a7
mikebennett <mvbennett9@gmail.com>
2022-09-01 22:51:18 +0900

    text will now reflect changes too

--- app/javascript/controllers/show_button_controller.js
+++ app/javascript/controllers/show_button_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['want', 'play', 'complete']
+  static targets = ['want', 'play', 'complete', 'content']
 
   change(event) {
     event.preventDefault()
@@ -11,23 +11,15 @@ export default class extends Controller {
     this.wantTarget[2].classList.add('btn-primary')
     this.playTarget[2].classList.add('btn-primary')
     this.completeTarget[2].classList.add('btn-primary')
-    // console.log(event.currentTarget[2]);
     event.currentTarget[2].classList.remove('btn-primary')
     event.currentTarget[2].classList.add('btn-outline-primary')
     event.currentTarget[2].blur()
-  }
-
-  play() {
-
-  }
-
-  complete() {
-
-  }
-
-  connect() {
-    console.log(this.wantTarget[2]);
-    console.log(this.playTarget[2]);
-    console.log(this.completeTarget[2]);
+    if (event.currentTarget[2].value === 'Want to play') {
+      this.contentTarget.innerText = "This game is on your shelf under want to play"
+    } else if (event.currentTarget[2].value === 'Currently playing') {
+      this.contentTarget.innerText = "This game is on your shelf under currently playing"
+    } else if (event.currentTarget[2].value === 'Completed') {
+      this.contentTarget.innerText = "This game is on your shelf under completed"
+    }
   }
 }
